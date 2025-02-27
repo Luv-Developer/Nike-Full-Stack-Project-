48a20fbd01823e5f98aa9cc2e7b3d64e9dac5ec4
user.name like:"Luv <bhardwaj.baljit@gmail.com>
2025-02-27 22:12:26 +0530

    added the backend database connection along with register page authentication and authorisation

--- app.js
+++ app.js
@@ -13,6 +13,33 @@ app.use(express.static(path.join(__dirname,"public")))
 app.get("/",(req,res)=>{
     res.render("homepage")
 })
+app.get("/register",(req,res)=>{
+    res.render("register")  
+})
+app.post("/register",async(req,res)=>{
+    const {username,email,password}  = req.body
+    const user = await usermodel.findOne({email})
+    if(user){
+        res.redirect("/")
+    }
+    else{
+        let saltround = await bcrypt.genSalt(10)
+        let hashedpassword = await bcrypt.hash(password,saltround)
+        let user = await usermodel.create({
+            username:username,
+            email:email,
+            password:hashedpassword
+        })
+        if(user){
+            let token = jwt.sign({email},"hehe")
+            res.cookie("token",token)
+            return res.redirect("/")
+        }
+        else{
+            return res.redirect("/register")
+        }
+    }
+})
 app.listen(PORT,()=>{
     console.log(`Server is listening at ${PORT}`)
 })
